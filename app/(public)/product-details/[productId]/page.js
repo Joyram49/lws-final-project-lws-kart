d78ba8c7c744acfc431339532ceecc3f40281c1f
Joy Ram <joyram2015@gmail.com>
2024-06-04 22:13:08 +0600

    update for metadata

--- app/(public)/product-details/[productId]/page.js
+++ app/(public)/product-details/[productId]/page.js
@@ -3,6 +3,15 @@ import Details from "@/components/productDetails/details/Details";
 import RelatedProductList from "@/components/productDetails/related/RelatedProductList";
 import { getProductById } from "@/lib/queries/products";
 
+export async function generateMetadata({ params }) {
+  const { productId } = params;
+  const product = await getProductById(productId);
+  return {
+    title: product.title,
+    description: product.description,
+  };
+}
+
 export const revalidate = 0;
 
 async function ProductDetailsPage({ params }) {
