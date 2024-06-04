0ae9859e9135a754c08c085dee201c2b33d9560b
Joy Ram <joyram2015@gmail.com>
2024-06-04 22:05:13 +0600

    update url for social sharing

--- components/common/ui/SocialSharing.jsx
+++ components/common/ui/SocialSharing.jsx
@@ -2,7 +2,7 @@
 import { FacebookShare, LinkedinShare, TwitterShare } from "react-share-kit";
 
 function SocialSharing({ title, productId, description, categoryName }) {
-  const shareUrl = `http://localhost:3000/product-details/${productId}`;
+  const shareUrl = `https://lws-final-project-lws-kart.vercel.app/product-details/${productId}`;
   return (
     <ul className='flex gap-3 mt-4'>
       <li className='text-gray-400 hover:text-gray-500 h-8 w-8 rounded-full border border-gray-300 flex items-center justify-center'>
